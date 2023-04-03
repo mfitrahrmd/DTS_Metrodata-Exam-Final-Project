0835c4ded45ba58a2faa8360fc1993c8dcf78200
Muhamad Fitrah Ramadhan <tgcfitrah26@gmail.com>
2023-04-03 23:54:05 +0700

    define user model

---
+++ Exam Final Project/models/User.cs
@@ -0,0 +1,28 @@
+namespace Exam_Final_Project.models;
+
+public class User
+{
+    public User()
+    {
+    }
+
+    public User(string username, string password, string fullname)
+    {
+        Username = username ?? throw new ArgumentNullException(nameof(username));
+        Password = password ?? throw new ArgumentNullException(nameof(password));
+        Fullname = fullname ?? throw new ArgumentNullException(nameof(fullname));
+    }
+
+    public int Id { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string Fullname { get; set; }
+
+    public override string ToString()
+    {
+        return $"Id : {Id}\n" +
+               $"Username : {Username}\n" +
+               $"Password : {Password}\n" +
+               $"Fullname : {Fullname}\n";
+    }
+}
