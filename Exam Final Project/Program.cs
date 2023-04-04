b6e34a4b74e281fd7d49f503cda1bd7707b3f583
Muhamad Fitrah Ramadhan <tgcfitrah26@gmail.com>
2023-04-04 14:58:53 +0700

    define main program and bootstrapping all dependencies

--- Exam Final Project/Program.cs
+++ Exam Final Project/Program.cs
@@ -1,3 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Exam_Final_Project.config.database;
+using Exam_Final_Project.controllers;
+using Exam_Final_Project.models;
+using Exam_Final_Project.repositories;
+using Exam_Final_Project.views;
 
-Console.WriteLine("Hello, World!");
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        try
+        {
+            // inject all the required dependencies
+            var sqlServerDb = new SqlServerDb("sa", "@DTS2023", "openmusic");
+            var userRepository = new UserRepository(sqlServerDb);
+            var albumRepository = new AlbumRepository(sqlServerDb);
+            var songRepository = new SongRepository(sqlServerDb);
+            var playlistRepository = new PlaylistRepository(sqlServerDb);
+            var playlistSongRepository = new PlaylistSongRepository(sqlServerDb);
+            var userController = new UserController(userRepository);
+            var albumController = new AlbumController(albumRepository);
+            var songController = new SongController(songRepository);
+            var playlistController = new PlaylistController(playlistRepository, playlistSongRepository);
+            var cliApp = new CLI(userController, albumController, songController, playlistController);
+            cliApp.Start();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
