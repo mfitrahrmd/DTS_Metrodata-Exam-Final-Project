ba52c423843fa4ebbde82dabc1eee15dd6b71713
Muhamad Fitrah Ramadhan <tgcfitrah26@gmail.com>
2023-04-04 14:47:38 +0700

    implement insert one, delete one by playlistid&songid , and find one by id with its songs methods

--- Exam Final Project/repositories/mssql/PlaylistSongRepository.cs
+++ Exam Final Project/repositories/mssql/PlaylistSongRepository.cs
@@ -1,3 +1,5 @@
+using System.Data;
+using System.Data.SqlClient;
 using Exam_Final_Project.config.database;
 using Exam_Final_Project.interfaces;
 using Exam_Final_Project.models;
@@ -12,11 +14,36 @@ public class PlaylistSongRepository : IPlaylistSongRepository
     {
         _sqlServerDb = sqlServerDb;
     }
-    
+
     // TODO : implement playlist_song repository
     public void InsertOne(PlaylistSong data)
     {
-        throw new NotImplementedException();
+        var rowAffected = _sqlServerDb.WithTx(command =>
+        {
+            command.CommandText = "INSERT INTO playlists_songs (playlist_id, song_id) VALUES (@playlist_id, @song_id);";
+            command.Parameters.AddRange(new[]
+            {
+                new SqlParameter
+                {
+                    ParameterName = "@playlist_id",
+                    SqlValue = data.PlaylistId,
+                    SqlDbType = SqlDbType.Int
+                },
+                new SqlParameter
+                {
+                    ParameterName = "@song_id",
+                    SqlValue = data.SongId,
+                    SqlDbType = SqlDbType.Int
+                }
+            });
+
+            return command;
+        });
+
+        if (rowAffected < 1)
+        {
+            throw new Exception("Failed to insert playlist_song");
+        }
     }
 
     public PlaylistSong FindOneById(int id)
@@ -37,5 +64,105 @@ public class PlaylistSongRepository : IPlaylistSongRepository
     public void DeleteOneById(int id)
     {
         throw new NotImplementedException();
+    }
+
+    public void DeleteOneByPlaylistIdAndSongId(int playlistId, int songId)
+    {
+        var rowAffected = _sqlServerDb.WithTx(command =>
+        {
+            command.CommandText =
+                "DELETE FROM playlists_songs WHERE playlist_id = @playlist_id AND song_id = @song_id;";
+            command.Parameters.AddRange(new[]
+            {
+                new SqlParameter
+                {
+                    ParameterName = "@playlist_id",
+                    SqlValue = playlistId,
+                    SqlDbType = SqlDbType.Int
+                },
+                new SqlParameter
+                {
+                    ParameterName = "@song_id",
+                    SqlValue = songId,
+                    SqlDbType = SqlDbType.Int
+                }
+            });
+
+            return command;
+        });
+
+        if (rowAffected < 1)
+        {
+            throw new Exception("Failed to delete playlist_song");
+        }
+    }
+
+    public PlaylistSongs FindOneByIdWithSongs(int playlistId)
+    {
+        PlaylistSongs playlistSongs = new PlaylistSongs(new List<Song>());
+        try
+        {
+            DataTable dataTablePlaylist = _sqlServerDb.WithDataTable(command =>
+            {
+                command.CommandText = "SELECT * FROM playlists WHERE id = @id;";
+                command.Parameters.Add(new SqlParameter
+                {
+                    ParameterName = "@id",
+                    SqlValue = playlistId,
+                    SqlDbType = SqlDbType.Int
+                });
+
+                return command;
+            });
+
+            if (dataTablePlaylist.Rows.Count < 1)
+            {
+                throw new Exception("Playlist not found");
+            }
+
+            playlistSongs.Id = dataTablePlaylist.Rows[0].Field<int>("id");
+            playlistSongs.Name = dataTablePlaylist.Rows[0].Field<string>("name");
+            playlistSongs.Owner = dataTablePlaylist.Rows[0].Field<int>("owner");
+
+            DataTable dataTable = _sqlServerDb.WithDataTable(command =>
+            {
+                command.CommandText =
+                    "SELECT p.*, s.id sid ,s.* FROM playlists p JOIN playlists_songs ps on p.id = ps.playlist_id JOIN songs s on ps.song_id = s.id WHERE p.id = @playlist_id;";
+                command.Parameters.Add(new SqlParameter
+                {
+                    ParameterName = "@playlist_id",
+                    SqlValue = playlistId,
+                    SqlDbType = SqlDbType.Int
+                });
+
+                return command;
+            });
+
+            if (dataTable.Rows.Count < 1)
+            {
+                throw new Exception("Playlist songs is empty");
+            }
+
+            foreach (DataRow row in dataTable.Rows)
+            {
+                playlistSongs.Songs.Add(new Song
+                {
+                    Id = row.Field<int>("sid"),
+                    Title = row.Field<string>("title"),
+                    Genre = row.Field<string>("genre"),
+                    Performer = row.Field<string>("performer"),
+                    Year = row.Field<Int16>("year"),
+                    Duration = row.Field<Int16>("duration"),
+                    IsExplicit = row.Field<bool>("is_explicit"),
+                    AlbumId = row.Field<int>("album_id")
+                });
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+        return playlistSongs;
     }
 }
